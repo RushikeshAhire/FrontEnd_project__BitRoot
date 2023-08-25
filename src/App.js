84b56ab8cc7c1bd5924ec13da4bcd14db119e272
Rushikesh Ahire <53400911+RushikeshAhire@users.noreply.github.com>
2023-08-25 09:13:27 +0530

    Add files via upload
    FrontEnt_Bitroot

---
+++ src/App.js
@@ -0,0 +1,33 @@
+
+
+
+
+function App() {
+ 
+  return(
+   
+   <>
+   <div>
+    <h1>Rushikesh Sunil ahire</h1>
+    <div>
+      <form>
+        <input type="text">Name:</input>
+        <input type="number">Number</input>
+      </form>
+    </div>
+   </div>
+   </>
+   
+
+  );
+    
+  
+};
+export default App;
+  
+     
+       
+  
+
+
+
